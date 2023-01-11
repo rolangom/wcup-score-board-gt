161cd4d2f7522ea80d4c0ac1eb859959c3ce0064
Rolando Gómez Tabar <rolangom@gmail.com>
2023-01-10 23:57:48 -0400

    Show main title
    Removed default template.
    Wrote main title as a heading text.
    Wrote test to validate.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders main header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleEl = screen.getByRole("heading", {
+    name: /football world cup score board/i,
+  });
+  expect(titleEl).toBeInTheDocument();
 });
